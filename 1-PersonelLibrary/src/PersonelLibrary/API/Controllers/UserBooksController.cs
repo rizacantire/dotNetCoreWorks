1701ee8f896d4b66236e383470483e4c42a41f46
rza35 <riza_tire@hotmail.com>
2022-02-09 15:28:55 +0300

    Üye kaydı, üyeye göre kitap ekleme ve listeleme eklendi.

--- 1-PersonelLibrary/src/PersonelLibrary/API/Controllers/UserBooksController.cs
+++ 1-PersonelLibrary/src/PersonelLibrary/API/Controllers/UserBooksController.cs
@@ -1,14 +1,18 @@
-﻿using Application.Contracts.Services;
+﻿using System.Security.Claims;
+using Application.Contracts.Services;
 using Application.Models.UsersBooks;
 using AutoMapper;
 using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Admin,User")]
     public class UserBooksController : ControllerBase
     {
         private readonly IUsersBooksService _usersBooksService;
@@ -19,7 +23,7 @@ namespace API.Controllers
             _usersBooksService = usersBooksService;
             _mapper = mapper;
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult Get()
         {
@@ -30,15 +34,38 @@ namespace API.Controllers
         [HttpPost]
         public IActionResult Add([FromBody] UsersBooksAddVm book)
         {
+            int userId = SignedUserId();
+            book.UserId = userId;
 
             return Ok(_usersBooksService.Add(_mapper.Map<UsersBooks>(book)));
         }
 
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
 
-            return Ok(_usersBooksService.GetByUserId(id));
+        [HttpGet("getByUser")]
+        public IActionResult GetByUser()
+        {
+            // var identityKey = string.Empty  ;
+            // if (HttpContext.User.Identity is ClaimsIdentity identity)
+            // {
+            //     identityKey = identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            // }
+
+             int userId = SignedUserId();
+  
+            return Ok(_usersBooksService.GetByUserId(userId));
         }
+
+        int SignedUserId()
+        {
+            var identityKey = string.Empty  ;
+            if (HttpContext.User.Identity is ClaimsIdentity identity)
+            {
+                identityKey = identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            }
+         
+            return int.Parse(identityKey);
+        }
+
+       
     }
 }
